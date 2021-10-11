2952403f6bd16dc8af569b50abcefd45d3901463
5undel <max.sundel@Choice.se>
2021-10-11 05:59:02 +0000

    adjust validation

--- run.py
+++ run.py
@@ -32,9 +32,10 @@ def validate_data(values):
 
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Exactly 6 values required, you provided{len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please typ again.\n")
